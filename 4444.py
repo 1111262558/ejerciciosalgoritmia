d7ad03f5facfed9c2ad4680011421347583238a7
william <whernandezgualtero@gmail.com>
2022-03-12 08:16:33 -0500

    actaulizacion1

---
+++ 4444.py
@@ -0,0 +1,6 @@
+c=int(input("dugite valor c  "))
+i=float(input("digite valor i  "))
+a=int(input("digite valor a  "))
+vf=c*(1+(i)/100)**(a)
+print("el valor de vf es:  ",vf)
+print(vf)
---
+++ 555.py
@@ -0,0 +1,5 @@
+b=int(input("digite valor de b  "))
+a=int(input("digite valor de a  "))
+area=a*b/2
+print("el valor de area es:  ",area)
+print(area)
